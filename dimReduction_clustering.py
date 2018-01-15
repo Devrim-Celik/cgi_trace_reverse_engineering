c60a1bafc322353a5d84da1b7b8e425e26cafce3
Devrim Celik <DevCel@hotmail.de>
2018-01-15 14:57:07 +0000

    now accepts pickled data with pandas dataframe inside

--- dimReduction_clustering.py
+++ dimReduction_clustering.py
@@ -1,79 +1,76 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import json
 from tsne import bh_sne
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
+from mpldatacursor import datacursor
+from prettytable import PrettyTable
 
-def dim_reduction(channel_dict, reduced_dim=2):
+def dim_reduction(main_df, reduced_dim=2, perplexity=30, n_kmeans_clusters=10):
+
+    # create a numpy array for voltage trace values
+    n_channels = len(main_df['Conc_Trace'])
+    n_trace_values = len(main_df['Conc_Trace'][0])
+    data_array = np.zeros((n_channels, n_trace_values))
+
+    # copy values
+    for row in range(n_channels):
+        data_array[row] = main_df['Conc_Trace'][row]
 
     """
     "we normalized each column by Z-scoring: we substracted its mean and
     then divided by its standard deviation"
-
-    since we are having a dictionary here, i will for now convert it into
-    a numpy array and at the end just assign it again to the dictionary keys
     """
-
-    n_channels = len(channel_dict.keys())
-    n_trace_values = len(list(channel_dict.values())[0])
-    data_array = np.zeros((n_channels, n_trace_values))
-
-    for counter, key in enumerate(channel_dict.keys()):
-        data_array[counter,:] = channel_dict[key]
-
     for column in range(n_trace_values):
         data_array[:,column] = (data_array[:,column] - \
                                 np.mean(data_array[:,column])) / \
                                 np.std(data_array[:,column])
 
+    # apply t-SNE on data
+    data2d = bh_sne(data_array, d=reduced_dim, perplexity=perplexity)
 
-
-################################################################################
-############################ DEMO FOR VISUALIZATION ############################
-################################################################################
-    # apply tsne with 5 different levels of perplexity
-    # and then apply kmeans clustering
-    # TODO Demo Version, find one perplexity you wanna use
-    kmeans = KMeans(n_clusters=20)
-    plt.figure("TSNE Results", figsize=(20,10))
-    print('\n\n\n\n\n\n START \n\n\n\n\n\n')
-    for counter, i in enumerate(range(5,46,10)):
-        print('\n\n\n HEHHEJKSHLHDLKAJSDH \n\n\n')
-        # dimensionality reduction
-        data2d = bh_sne(data_array, perplexity=i)
-
-        # apply clustering and retrieve resulting labels
-        kmeans.fit(data2d)
-        if counter == 0:
-            labels = kmeans.labels_
-
-        # generate plot
-        plt.subplot(2,3,counter+1)
-        plt.title("t-SNE with perplexity of " + str(i))
-        plt.scatter(data2d[:,0], data2d[:,1], linewidths=0.1, c=labels)
-
-
-    # apply pca for comparison
-    pca = PCA(n_components=2)
-    data2d = pca.fit_transform(data_array)
-
+    # apply kmeans and save its labels for colorization
+    kmeans = KMeans(n_clusters=n_kmeans_clusters)
     kmeans.fit(data2d)
     labels = kmeans.labels_
 
-    plt.subplot(2,3,6)
-    plt.title("PCA")
-    plt.scatter(data2d[:,0], data2d[:,1], linewidths=0.1, c=labels)
+    # create list of labels, which will be displayed when clicking
+    # on a datapoint
+    text_list = [create_label_for_matplotlib(main_df.loc[counter])      \
+    for counter in range(n_channels)]
 
-    # plot results
+    # plot
+    plt.figure("Interactive Plot", figsize=(20,10))
+    plt.title("Interactive Plot of Channels, click on Datapoints for Info")
+
+    # for assigning different colors to different clusters
+    nr_to_color = ["b", "g", "r", "c", "y", "m", "k", "fuchsia",        \
+                    "gray", "navy", "coral"]
+
+    # plot every datapoint with its corresponding text ("label")
+    # dont iterative, to assign labels correctly
+    for dp in range(n_channels):
+        plt.scatter(data2d[dp,0], data2d[dp,1], linewidths=0.1,         \
+        c=nr_to_color[labels[dp]], label=text_list[dp])
+
+    # add datacursor, basically enabling "clickability" of datapoints
+    datacursor(formatter='{label}'.format, bbox=dict(fc='white'),       \
+                 arrowprops=dict(arrowstyle='simple', fc='black', alpha=0.5))
+
     plt.show()
-################################################################################
-################################################################################
-
-    data2d = bh_sne(data_array, d=reduced_dim, perplexity=30)
-
-    # TODO transform into panda dataframe or something, keep the ids!
     return 0
+
+def create_label_for_matplotlib(series_of_dp):
+    label = PrettyTable(["Attribute", "Value"])
+    exclude = ["Conc_Trace", "Temperature"]
+    for attribute in series_of_dp.keys():
+        if (attribute not in exclude):
+            label.add_row([attribute, series_of_dp[attribute]])
+
+
+    return str(label)
 
 if (__name__=="__main__"):
 
@@ -82,8 +79,11 @@ if (__name__=="__main__"):
     ion_dim = {"K":16, "Na":21, "Ca":29, "IH":16, "KCa":16}
 
     # Get traces
-    file_name = "Na_family.json"
+    file_name = "Na_family_dataframe.pickle"
+    """
     with open(file_name, 'r') as f:
         channel_dict = json.load(f)
+    """
 
-    pp_channel_dict = dim_reduction(channel_dict)
+    main_df = pd.read_pickle(file_name)
+    pp_channel_dict = dim_reduction(main_df)
